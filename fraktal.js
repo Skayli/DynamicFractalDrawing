b74bcee085d2c07609fa717a5f169ba1e4c74472
quentinnade <quentin.nade27@gmail.com>
2018-09-16 20:26:07 +0200

    Initial commit

---
+++ fraktal.js
@@ -0,0 +1,146 @@
+var defaultxMin, defaultxMax, defaultyMin, defaultyMax;
+var xMin, xMax, yMin, yMax;
+var zoomLevel;
+var canvas, canvasWidth, canvasHeight, ctx;
+var complex;
+var nbIterations;
+var palette;
+
+$(document).ready(function() {
+	canvas = document.getElementById('canvas');
+	canvasWidth  = canvas.width;
+	canvasHeight = canvas.height;
+	ctx = canvas.getContext('2d');
+
+	defaultxMin = -3;
+	defaultxMax = 3;
+	defaultyMin = -1.5;
+	defaultyMax = 1.5;
+
+	nbIterations = 100;
+	palette = [];
+
+	generatePalette();
+
+
+	//----------------------------------------//
+
+	$("#zoomLevel").on('input', function() {
+		$("#rangeValue").html($(this).val());
+	})
+
+	$("#fraktalForm").submit(function(e) {
+		e.preventDefault();
+
+		$(".fraktalIntro").hide();
+		$(".fraktalCanvas").hide();
+		$(".fraktalLoading").show();
+
+		getUserValues();
+
+		setTimeout(dessinerFractale, 10);
+	});
+
+});
+
+function generatePalette() {
+    // Calculate a gradient
+    var roffset = 24;
+    var goffset = 16;
+    var boffset = 0;
+    for (var i=0; i<256; i++) {
+        palette[i] = { r:roffset, g:goffset, b:boffset};
+
+        if (i < 64) {
+            roffset += 3;
+        } else if (i<128) {
+            goffset += 3;
+        } else if (i<192) {
+            boffset += 3;
+        }
+    }
+}
+
+
+function initDataInInputs() {
+	$("#re").val(complex.re);
+	$("#im").val(complex.im);
+}
+
+function getUserValues() {
+	zoomLevel = $("#zoomLevel").val();
+
+	xMin = defaultxMin / zoomLevel;
+	xMax = defaultxMax / zoomLevel;
+	yMin = defaultyMin / zoomLevel;
+	yMax = defaultyMax / zoomLevel;
+
+	let reel = parseFloat($("#re").val());
+	let imaginaire = parseFloat($("#im").val());
+
+	complex = math.complex(reel, imaginaire);
+}
+
+function dessinerFractale() {
+	for(i = 0; i < canvas.width; i++) {;
+		for(j = 0; j < canvas.height; j++) {
+			colorPixel(ctx, i, j);
+		}
+	}
+
+
+	$(".fraktalCanvas").show();
+	$(".fraktalLoading").hide();
+
+}
+
+function moduleComplex(complex) {
+	return math.sqrt(math.multiply(complex.re, complex.re) + math.multiply(complex.im, complex.im));
+}
+
+function colorPixel(ctx, x, y) {
+	let color = defineColorForPixel(x, y, complex)
+	ctx.fillStyle = color;
+	ctx.fillRect(x, y, 1, 1);
+}
+
+function r() {
+	return Math.floor(Math.random() * 255);
+}
+
+function pixelToComplex(x, y) {
+	return math.complex(xMin + (x - 0) * ( (xMax - xMin) / (canvasWidth - 0) ), yMin + (y - 0) * ( (yMax - yMin) / (canvasHeight - 0) ) );
+}
+
+function complexToPixel(complex) {
+	var coord = [];
+	coord[0] = math.floor(0 + (complex.re - xMin) * ( (canvasWidth - 0) / (xMax - xMin) ));
+	coord[y] = math.floor(0 + (complex.im - yMin) * ( (canvasHeight - 0) / (yMax - yMin) ));
+
+	return coord;
+}
+
+function carreComplex(complex) {
+	return math.complex(math.multiply(complex.re, complex.re) - math.multiply(complex.im, complex.im), 2*complex.re*complex.im);
+}
+
+function defineColorForPixel(x, y, c) {
+	var compteur = 0;
+	var u = pixelToComplex(x, y); //terme de la suite
+
+	while(compteur < nbIterations && moduleComplex(u) < 2) {
+		u = math.add(carreComplex(u), c);
+		compteur++;
+	}
+
+	var color;
+	if(compteur == nbIterations) {
+		color = { r:0, g:0, b:0}; // Black
+	} else {
+		var index = Math.floor((compteur / (nbIterations-1)) * 255);
+        color = palette[index];
+	}
+
+	return "rgba("+color.r+", "+color.g+", "+color.b+",1)";
+
+}
---
+++ index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+	<script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/mathjs/5.1.2/math.min.js"></script>
+	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+
+	<script src="fraktal.js"></script>
+	<link rel="stylesheet" href="fraktal.css" />
+</head>
+
+<body>
+	<div class="w3-container">
+		<h1 class="w3-text-white w3-center w3-xlarge title">Fraktal with style</h1>
+	</div>
+
+	<!-- Container du dessin de la fractale -->
+	<div class="w3-container fraktalDrawing">
+		<div class="fraktalContent">
+			<!-- Message d'introduction du site -->
+			<div class="fraktalIntro">
+				<h2 class="w3-center" id="welcomeTitle">Bienvenue sur Fraktal with style</h2>
+				<p>
+					Ici, il faut décrire ce que les gens peuvent faire avec le form en dessous :D
+				</p>
+			</div>
+
+			<!-- canvas -->
+			<div class="fraktalCanvas">
+				<canvas id="canvas" width="1200" height="600">Si vous voyez ceci, mettez à jour ou changer de navigateur pour voir les fractales</canvas>
+			</div>
+
+			<!-- Message de loading -->
+			<div class="w3-container fraktalLoading">
+				<h2 class="w3-center" id="welcomeTitle">Dessin de la fractal en cours</h2>
+				<p>
+					Veuillez patienter
+				</p>
+				<div class="w3-light-grey">
+					<div id="loadingBar" class="w3-container w3-grey" style="height:24px;width:0%;"></div>
+				</div>
+			</div>
+		</div>
+
+		<!-- Formulaire permettant de remplir les données concernant le dessin de la fractale -->
+		<form class="w3-section fraktalForm" id="fraktalForm">
+
+			<!-- Zoom -->
+			<div class="w3-container">
+				<span class="fraktalForm-desc">Niveau de zoom</span>
+				<span class="fraktalForm-desc" id="zoomLevelDisplay">X<span id="rangeValue">1</span></span>
+			</div>
+			<div class="w3-container">
+				<input class="w3-input fraktalForm-input" type="range" min="0.5" max="10" value="1" name="zoomLevel" id="zoomLevel" step="0.5" />
+			</div>
+
+			<!-- Partie réel du complexe -->
+			<div class="w3-container w3-margin-top">
+				<span class="fraktalForm-desc">Partie réelle</span>
+			</div>
+			<div class="w3-container">
+				<input class="w3-input fraktalForm-input" type="number" step="any" name="re" id="re" value="0.285"/>
+			</div>
+
+			<!-- Partie imaginaire du complexe -->
+			<div class="w3-container w3-margin-top">
+				<span class="fraktalForm-desc">Partie imaginaire</span>
+			</div>
+			<div class="w3-container">
+				<input class="w3-input fraktalForm-input" type="number" step="any" name="im" id="im" value="0.01"/>
+			</div>
+
+			<!-- Bouton pour tracer la fractale -->
+			<div class="w3-margin-top">
+				<button class="w3-btn w3-red" id="btn-draw">Tracer</button>
+			</div>
+
+		</form>
+
+	</div>
+
+</body>
+
+</html>
